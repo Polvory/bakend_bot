b3067f26831a0747152a79df834d953f71e3c617
Polvory <32514592+Polvory@users.noreply.github.com>
2023-06-10 18:38:14 +0300

    Подключил к бд

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { SequelizeModule } from '@nestjs/sequelize';
+import {DB_NAME, DB_PASSWORD, DB_USER, DB_PORT} from './utils/dev'
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: DB_PORT,
+      username: DB_USER,
+      password: DB_PASSWORD,
+      database: DB_NAME,
+      models: [ 
+
+      ],
+      autoLoadModels: true
+    }),
+  ],
+
+
 })
-export class AppModule {}
+export class AppModule { }
